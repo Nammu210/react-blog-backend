2db7f22317044ddecea167220ad775465e37bb93
Nammu210 <namratajas2020@gmail.com>
2023-09-14 15:49:27 +0530

    fg

--- src/Components/Advertisement/Advertise.js
+++ src/Components/Advertisement/Advertise.js
@@ -5,15 +5,21 @@ const Advertisement = () => {
   return (
     <div className='Ad'>
         
-        <iframe width="300" height="150"
+        <iframe width="350" height="150"
                 src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1" >
-                </iframe>
-                <iframe width="300" height="150"
+        </iframe>
+                
+        <iframe width="350" height="150"
                 src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1" >
-                </iframe>
-                <iframe width="300" height="150"
+        </iframe>
+
+        <iframe width="350" height="150"
                 src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1" >
-                </iframe>
+        </iframe>
+
+
+                
+                
     </div>
   )
 }
